dbb228bba3a255b99024a547a2ad26d2001fbf75
tatidem <tdemyanova@mail.ru>
2023-12-24 19:34:49 +0400

    feat: add answers

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  transform(message, key, encrypt) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const messageChar = message[i];
+
+      if (this.alphabet.includes(messageChar)) {
+        const messageIndex = this.alphabet.indexOf(messageChar);
+        const keyChar = key[keyIndex % key.length];
+        const keyShift = encrypt ? this.alphabet.indexOf(keyChar) : -this.alphabet.indexOf(keyChar);
+        const transformedIndex = (messageIndex + keyShift + this.alphabet.length) % this.alphabet.length;
+        const transformedChar = this.alphabet[transformedIndex];
+        result += transformedChar;
+        keyIndex++;
+      } else {
+        result += messageChar;
+      }
+    }
+
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  encrypt(message, key) {
+    return this.transform(message, key, true);
+  }
+
+  decrypt(encryptedMessage, key) {
+    return this.transform(encryptedMessage, key, false);
   }
 }
 
